e41ec2cc1c1db074423ea07a64550262c6087630
Joey-2134 <joeygalvin2134@gmail.com>
2024-05-02 23:38:55 +0100

    Initial commit

---
+++ src/main/java/net/joey/betterfarming/BetterFarming.java
@@ -0,0 +1,16 @@
+package net.joey.betterfarming;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterFarming implements ModInitializer {
+	public static final String MOD_ID = "betterfarming";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/joey/betterfarming/BetterFarmingClient.java
@@ -0,0 +1,10 @@
+package net.joey.betterfarming;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterFarmingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
